f65d7d20f042e8b26d56adeadf03b7d1c2a70368
Andy Brett <andy@andybrett.com>
2021-06-29 09:56:51 -0700

    update for rails 6.1

--- lib/rails/extensions/active_model.rb
+++ lib/rails/extensions/active_model.rb
@@ -3,24 +3,22 @@ module ActiveModel
     def full_messages
       full_messages = []
 
-      each do |attribute, messages|
-        messages = Array.wrap(messages)
-        next if messages.empty?
+      each do |error|
+        attribute = error.attribute
+        message = error.message
+        next if message.empty?
 
         if attribute == :base
-          messages.each {|m| full_messages << m }
-        else          
+          full_messages << message
+        else
           attr_name = attribute.to_s.gsub('.', '_').humanize
           attr_name = @base.class.human_attribute_name(attribute, :default => attr_name)
           options = { :default => "%{attribute} %{message}", :attribute => attr_name }
 
-          
-          messages.each do |m|
-            if m =~ /^\^/
-              full_messages << I18n.t(:"errors.format.full_message", options.merge(:message => m[1..-1], :default => "%{message}"))
-            else        
-              full_messages << I18n.t(:"errors.format", options.merge(:message => m))
-            end
+          if message =~ /^\^/
+            full_messages << I18n.t(:"errors.format.full_message", options.merge(:message => message[1..-1], :default => "%{message}"))
+          else
+            full_messages << I18n.t(:"errors.format", options.merge(:message => message))
           end
         end
       end
